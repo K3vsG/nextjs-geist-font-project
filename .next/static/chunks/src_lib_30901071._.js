4750a41966a996e5eb84e2ce865ce45b07d707dc
Kevin Sanz ᓚᘏᗢ <142844183+K3vsG@users.noreply.github.com>
2025-08-01 11:02:14 -0600

    Add .next/static/chunks/src_lib_30901071._.js 1754067734016

---
+++ .next/static/chunks/src_lib_30901071._.js
@@ -0,0 +1,23 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([typeof document === "object" ? document.currentScript : undefined, {
+
+"[project]/src/lib/mock-data.ts [app-client] (ecmascript, async loader)": ((__turbopack_context__) => {
+
+var { g: global, __dirname } = __turbopack_context__;
+{
+__turbopack_context__.v((parentImport) => {
+    return Promise.resolve().then(() => {
+        return parentImport("[project]/src/lib/mock-data.ts [app-client] (ecmascript)");
+    });
+});
+}}),
+"[project]/src/lib/visits.ts [app-client] (ecmascript, async loader)": ((__turbopack_context__) => {
+
+var { g: global, __dirname } = __turbopack_context__;
+{
+__turbopack_context__.v((parentImport) => {
+    return Promise.resolve().then(() => {
+        return parentImport("[project]/src/lib/visits.ts [app-client] (ecmascript)");
+    });
+});
+}}),
+}]);
